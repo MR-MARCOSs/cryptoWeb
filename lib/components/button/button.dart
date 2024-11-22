a3a36f638fd057bb3abdb9dbbbdaea4e24f6586c
Marcos Eduardo <marcosdudu001@gmail.com>
2024-11-22 10:01:19 -0300

    changes in button component and samples screens added

--- lib/components/button/button.dart
+++ lib/components/button/button.dart
@@ -8,22 +8,29 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: viewModel.onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: viewModel.fillColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0), // Cantos arredondados
+    return SizedBox(
+      width: viewModel.width, // Define a largura do botão
+      height: viewModel.height, // Define a altura do botão
+      child: ElevatedButton(
+        onPressed: viewModel.onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: viewModel.fillColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0), // Cantos arredondados
+            side: viewModel.hasBorder
+                ? BorderSide(
+                    color: viewModel.borderColor,
+                    width: viewModel.borderWidth,  // Usando a largura da borda
+                  )
+                : BorderSide.none, // Sem borda se hasBorder for false
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0), // Padding interno
         ),
-        side: viewModel.hasBorder
-            ? BorderSide(color: viewModel.borderColor)
-            : BorderSide.none, // Sem borda se hasBorder for false
-        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0), // Padding interno
-      ),
-      child: Text(
-        viewModel.buttonText,
-        style: TextStyle(
-          color: viewModel.textColor, // Cor do texto, contraste com fillColor
+        child: Text(
+          viewModel.buttonText,
+          style: TextStyle(
+            color: viewModel.textColor, // Cor do texto, contraste com fillColor
+          ),
         ),
       ),
     );
--- lib/components/button/button_view_model.dart
+++ lib/components/button/button_view_model.dart
@@ -7,8 +7,9 @@ class ButtonViewModel {
   final Color borderColor;
   final VoidCallback? onPressed;
   final Color textColor;
-
-
+  final double borderWidth;  // Nova propriedade para o tamanho da borda
+  final double? width;
+  final double? height;
 
   ButtonViewModel({
     required this.buttonText,
@@ -17,5 +18,8 @@ class ButtonViewModel {
     this.borderColor = Colors.grey,  // Cor da borda se houver
     required this.onPressed,
     this.textColor = Colors.white, // Cor do texto padrão (branco)
+    this.borderWidth = 2.0, // Valor padrão para a largura da borda
+    this.width,
+    this.height,
   });
 }
