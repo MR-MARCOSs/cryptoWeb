73b4227d816bdf9d246b9a16b9ab4baf5ed408dc
MR-MARCOSs <marcosdudu001@gmail.com>
2024-11-22 17:05:06 -0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'components/button/button.dart';
+import 'components/button/button_view_model.dart';
+import 'components/input_field/input_field.dart';
+import 'components/input_field/input_field_view_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +11,148 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cadastrar Tela',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.deepPurple, // Cor principal
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CadastrarTela(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CadastrarTela extends StatefulWidget {
+  const CadastrarTela({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CadastrarTela> createState() => _CadastrarTelaState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CadastrarTelaState extends State<CadastrarTela> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+  final TextEditingController _confirmarSenhaController =
+      TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool _aceitoTermos = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.deepPurple, // Cor de fundo da tela
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Card(
+          margin: const EdgeInsets.all(20.0),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  'Cadastrar',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.grey, // Cor de fundo do avatar
+                ),
+                const SizedBox(height: 20),
+                InputField(
+                  viewModel: InputFieldViewModel(
+                    controller: _emailController,
+                    hintText: 'Email ou telefone',
+                    fillColor: Colors.grey[200]!, // Cor de fundo do input
+                  ),
+                ),
+                const SizedBox(height: 10),
+                InputField(
+                  viewModel: InputFieldViewModel(
+                    controller: _senhaController,
+                    hintText: 'Senha',
+                    obscureText: true,
+                    fillColor: Colors.grey[200]!, // Cor de fundo do input
+                  ),
+                ),
+                const SizedBox(height: 10),
+                InputField(
+                  viewModel: InputFieldViewModel(
+                    controller: _confirmarSenhaController,
+                    hintText: 'Confirmar senha',
+                    obscureText: true,
+                    fillColor: Colors.grey[200]!,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _aceitoTermos,
+                      onChanged: (value) {
+                        setState(() {
+                          _aceitoTermos = value!;
+                        });
+                      },
+                    ),
+                    const Text('Aceito os '),
+                    InkWell(
+                      onTap: () {
+                        // Ação ao clicar em "Termos de Uso"
+                      },
+                      child: const Text(
+                        'Termos de Uso',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+
+                const SizedBox(height: 5),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Já tem conta?'),
+                    InkWell(
+                      onTap: () {
+                        // Ação ao clicar em "Fazer Login"
+                      },
+                      child: const Text(
+                        ' Fazer Login',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+
+
+                const SizedBox(height: 20),
+                Button(
+                  viewModel: ButtonViewModel(
+                    buttonText: 'Continuar',
+                    fillColor: Colors.deepPurple,
+                    onPressed: () {
+                      // Ação ao clicar em "Continuar"
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
